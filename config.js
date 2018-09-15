9fe2f10c4771b619a0b3adbdb2989b0773dbf2e7
Luke Murray <LukeusMaximus@users.noreply.github.com>
2018-09-15 21:34:29 +0100

    Initial upload of web files.

---
+++ config.js
@@ -0,0 +1,158 @@
+export default {
+	date: "2018-09-22",
+	schedule: [
+		{
+			starting: "0930",
+			turn: "Welcome"
+			phase: "Arrival and registration"
+		},
+		{
+			starting: "1000",
+			turn: "Briefing",
+			phase: "Welcome and Introductory Briefing"
+		},
+		{
+			starting: "1015",
+			turn: "Planning",
+			phase: "Planning for turn 1"
+		},
+
+		{
+			starting: "1030",
+			turn: "Turn 1",
+			phase: "Action Phase"
+		},
+		{
+			starting: "1045",
+			turn: "Turn 1",
+			phase: "Team Phase"
+		},
+
+		{
+			starting: "1100",
+			turn: "Turn 2",
+			phase: "Action Phase"
+		},
+		{
+			starting: "1115",
+			turn: "Turn 2",
+			phase: "Team Phase"
+		},
+
+		{
+			starting: "1130",
+			turn: "Turn 3",
+			phase: "Action Phase"
+		},
+		{
+			starting: "1145",
+			turn: "Turn 3",
+			phase: "Team Phase"
+		},
+
+		{
+			starting: "1200",
+			turn: "Turn 4",
+			phase: "Action Phase"
+		},
+		{
+			starting: "1215",
+			turn: "Turn 4",
+			phase: "Team Phase"
+		},
+
+		{
+			starting: "1230",
+			turn: "Turn 5",
+			phase: "Action Phase"
+		},
+		{
+			starting: "1245",
+			turn: "Turn 5",
+			phase: "Team Phase"
+		},
+
+		{
+			starting: "1300",
+			turn: "Turn 6",
+			phase: "Action Phase"
+		},
+		{
+			starting: "1315",
+			turn: "Turn 6",
+			phase: "Team Phase"
+		},
+
+		{
+			starting: "1330",
+			turn: "Turn 7",
+			phase: "Action Phase"
+		},
+		{
+			starting: "1345",
+			turn: "Turn 7",
+			phase: "Team Phase"
+		},
+
+		{
+			starting: "1400",
+			turn: "Turn 8",
+			phase: "Action Phase"
+		},
+		{
+			starting: "1415",
+			turn: "Turn 8",
+			phase: "Team Phase"
+		},
+
+		{
+			starting: "1430",
+			turn: "Turn 9",
+			phase: "Action Phase"
+		},
+		{
+			starting: "1445",
+			turn: "Turn 9",
+			phase: "Team Phase"
+		},
+
+		{
+			starting: "1500",
+			turn: "Turn 10",
+			phase: "Action Phase"
+		},
+		{
+			starting: "1515",
+			turn: "Turn 10",
+			phase: "Team Phase"
+		},
+
+		{
+			starting: "1530",
+			turn: "Turn 11",
+			phase: "Action Phase"
+		},
+		{
+			starting: "1545",
+			turn: "Turn 11",
+			phase: "Team Phase"
+		},
+
+		{
+			starting: "1600",
+			turn: "Turn 12",
+			phase: "Action Phase"
+		},
+		{
+			starting: "1615",
+			turn: "Turn 12",
+			phase: "Team Phase"
+		},
+
+		{
+			starting: "1630",
+			turn: "Debriefing",
+			phase: "The game is over."
+		},
+	]
+};
---
+++ turnometer.js
@@ -0,0 +1,12 @@
+import config from "config.js"
+
+const $time = document.querySelector("#time");
+const $turn = document.querySelector("#turn");
+const $phase = document.querySelector("#phase");
+
+function update() {
+	
+}
+
+update();
+setInterval(update, 1000);
