8a6d24e1969a56ed8191fd27676ce62decca5fcd
Luke Murray <github@lukemurray.co.uk>
2018-09-16 15:05:52 +0100

    Switched to using SVG to perform desired text scaling.

--- turnometer.js
+++ turnometer.js
@@ -1,20 +1,52 @@
 import config from "./config.js"
 
 const $time = document.querySelector("#time");
+const $timeContainer = document.querySelector("#time-container");
 const $turn = document.querySelector("#turn");
+const $turnContainer = document.querySelector("#turn-container");
 const $phase = document.querySelector("#phase");
+const $phaseContainer = document.querySelector("#phase-container");
+
+function formatCurrentTimeFromDate(date) {
+	let hour = `${date.getHours()}`;
+    let minute = `${date.getMinutes()}`;
+    let second = `${date.getSeconds()}`;
+	if(hour.length === 1) {
+		hour = `0${hour}`;
+	}
+    if(minute.length === 1) {
+        minute = `0${minute}`;
+    }
+    if(second.length === 1) {
+        second = `0${second}`;
+    }
+    return `${hour}:${minute}:${second}`;
+}
+
+// Sets the text of an SVG element then makes the element fill the SVG container perfectly
+const TEXT_EDGE_SPACING = 1;
+
+function setSVGText($container, $text, str) {
+	$text.textContent = str;
+	const bbox = $text.getBBox();
+	const viewbox = $container.viewBox.baseVal;
+	viewbox.x = bbox.x - TEXT_EDGE_SPACING;
+	viewbox.y = bbox.y - TEXT_EDGE_SPACING;
+	viewbox.width = bbox.width + 2 * TEXT_EDGE_SPACING;
+	viewbox.height = bbox.height + 2 * TEXT_EDGE_SPACING;
+}
 
 function update() {
 	const now = new Date();
-	$time.innerText = `${now.getHours()}:${now.getMinutes()}:${now.getSeconds()}`;
+    setSVGText($timeContainer, $time, formatCurrentTimeFromDate(now));
 
 	if(now < config.startTime || now > config.endTime) {
-		$turn.innerText = "Game not in progress";
-		$phase.innerText = "We'd love to megagame every day but alas we cannot.";
+		setSVGText($turnContainer, $turn, "Game not in progress");
+		setSVGText($phaseContainer, $phase, "We'd love to megagame every day but alas we cannot.");
 	} else {
 		const currentPeriod = config.schedule.find(({starting}) => starting <= now);
-		$turn.innerText = currentPeriod.turn;
-		$phase.innerText = currentPeriod.phase;
+        setSVGText($turnContainer, $turn, currentPeriod.turn);
+        setSVGText($phaseContainer, $phase, currentPeriod.phase);
 	}
 }
 
